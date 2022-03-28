14929b0bbd3083f266f2eb02dd385f0629f63c05
quangchinh186 <quangchinh1122@gmail.com>
2022-03-28 15:36:18 +0700

    SDL basic stuff

---
+++ DSL_testing_function/main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+
+using namespace std;
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const string WINDOW_TITLE = "My Game";
+
+//bool Painter::createImage( SDL_Texture* texture )
+void logSDLError(std::ostream& os,
+                 const std::string &msg, bool fatal = false);
+
+void logSDLError(std::ostream& os,
+                 const std::string &msg, bool fatal)
+{
+    os << msg << " Error: " << SDL_GetError() << std::endl;
+    if (fatal) {
+        SDL_Quit();
+        exit(1);
+    }
+}
+
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer);
+SDL_Texture* loadTexture(string path, SDL_Renderer* renderer );
+
+int main(int argc, char* argv[])
+{
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    initSDL(window, renderer);
+    SDL_rect object;
+
+
+    SDL_Texture* pacman = loadTexture("pacman.gif", renderer);
+    SDL_RenderCopy(renderer, pacman, 0, 0);
+    SDL_RenderPresent(renderer);
+
+    SDL_Event e;
+    while (true) {
+        if ( SDL_WaitEvent(&e) == 0){SDL_Delay(100);}
+        else if(e.type == SDL_QUIT){break;}
+        else if(e.type == SDL_KEYDOWN){
+            cout << "_" << SDL_GetKeyName(e.key.keysym.sym) << "_" << endl;
+        }
+    }
+
+    quitSDL(window, renderer);
+    return 0;
+}
+
+SDL_Texture* loadTexture(string path, SDL_Renderer* renderer )
+{
+    SDL_Texture* newTexture = nullptr;
+    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+    if ( loadedSurface == nullptr )
+        cout << "Unable to load image " << path << " SDL_image Error: "
+             << IMG_GetError() << endl;
+    else {
+        newTexture = SDL_CreateTextureFromSurface( renderer, loadedSurface );
+        if( newTexture == nullptr )
+            cout << "Unable to create texture from " << path << " SDL Error: "
+                 << SDL_GetError() << endl;
+        SDL_FreeSurface( loadedSurface );
+    }
+    return newTexture;
+}
+
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
+{
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+        logSDLError(std::cout, "SDL_Init", true);
+
+    window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr) logSDLError(std::cout, "CreateWindow", true);
+
+    //Khi chạy trong môi trường bình thường (không chạy trong máy ảo)
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+                                              SDL_RENDERER_PRESENTVSYNC);
+    //Khi chạy ở máy ảo (ví dụ tại máy tính trong phòng thực hành ở trường)
+    //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
+
+    if (renderer == nullptr) logSDLError(std::cout, "CreateRenderer", true);
+
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+}
+
